327a70609ec34fdf083e69be539d7d8f64a34c34
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-03-28 20:09:10 +0300

    i only added this library react-alice-carousel

---
+++ src/Alice.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import data from "./data";
+import "./style.css";
+
+class Alice extends React.Component { 
+
+  responsive={
+    0:{items:2},
+    600:{items:2},
+    900:{items:3},
+    1024:{items:4},
+  };
+
+  galleryItems() {
+    return (
+      data.properties.map((item, i) => (
+        <div key={`key-${i}`} className="slide-item"><h3>{item.name}</h3>
+        <img style={{width:"100px",height:"100px"}} src={item.picture} alt="..."/>
+        
+        </div>
+      ))
+    )
+  };
+
+ 
+render(){
+  const items = this.galleryItems();
+
+  return (
+    <div>
+          <AliceCarousel
+          style={{color:"red"}}
+        items={items}
+        duration={400}
+        autoPlay={true}
+        startIndex = {1}
+        fadeOutAnimation={true}
+        mouseDragEnabled={true}
+        playButtonEnabled={false}
+        buttonsDisabled={true}
+        dotsDisabled={true}
+        autoPlayInterval={2000}
+        autoPlayDirection="rtl"
+        responsive={this.responsive}
+        disableAutoPlayOnAction={false}
+       
+      />
+    </div>
+  )
+}
+
+}
+
+export default Alice;
---
+++ src/data.js
@@ -0,0 +1,121 @@
+import one from './assets/logos/8.png';
+import two from './assets/logos/Atiak Technical Sch-Amuru.jpg';
+import three from './assets/logos/Bobi Community Polytechnic.jpg';
+import four from './assets/logos/Bukalasa agric college.jpg';
+import five from './assets/logos/centenery community college.jpg';
+import six from './assets/logos/Daniel Comboni Vocational Institute.jpg';
+import seven from './assets/logos/Flaminio Vocational Training Contre.jpg';
+import eight from './assets/logos/Human Technical Development Training Centre.jpg';
+import nine from './assets/logos/Innovative Institute of Agriculture, business and Capacity (IACB).jpg';
+import ten from './assets/logos/masterfoundation.png';
+import eleven from './assets/logos/Mityana Agro Vet Institute.jpg';
+import twelve from './assets/logos/St.Thereza Vocational Training Centre.jpg';
+import thirteen from './assets/logos/Stride.jpg';
+import forteen from './assets/logos/Technoserve.jpg';
+
+
+const data = {
+    "properties": [
+    {
+        "_id": "593e92977e17df20c4a237d42",
+        "index": 0,
+        "name": "#####",
+        "picture": one,
+        
+    },
+    {
+        "_id": "593e9297e17df720c4a237d42",
+        "index": 1,
+        "name": "Atiak Technical Sch-Amuru",
+        "picture": two,
+        
+    },
+    
+    {
+        "_id": "593e9297e17df20c4a2737d42",
+        "index":2 ,
+        "name": "Bobi Community Polytechnic",
+        "picture": three,
+        
+    },
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 3,
+        "name": "Bukalasa agric college",
+        "picture": four,
+        
+    },
+    {
+        "_id": "593e9297e17df20c4a2737d42",
+        "index": 4,
+        "name": "centenery community college",
+        "picture": five,
+        
+    },
+    {
+        "_id": "593e92797e17df20c4a237d42",
+        "index": 5,
+        "name": "Daniel Comboni Vocational Institute",
+        "picture": six,
+        
+    },
+    {
+        "_id": "593e9297e17df2h0c4a237d42",
+        "index": 6,
+        "name": "Flaminio Vocational Training Contre",
+        "picture": seven,
+        
+    },
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 7,
+        "name": "Human Technical Development Training Centre",
+        "picture": eight,
+        
+    },
+    {
+        "_id": "593e92r97e17df20c4a237d42",
+        "index": 8,
+        "name": "Innovative Institute of Agriculture, business and Capacity (IACB)",
+        "picture": nine,
+        
+    },
+    {
+        "_id": "593e92e97e17df20c4a237d42",
+        "index": 9,
+        "name": "Mastercard foundation",
+        "picture": ten,
+        
+    },
+    {
+        "_id": "593e9297e17df20c4a237d42",
+        "index": 10,
+        "name": "Mityana Agro Vet Institute",
+        "picture": eleven,
+        
+    },
+    {
+        "_id": "593e9297e1i7df20c4a237d42",
+        "index": 11,
+        "name": "St.Thereza Vocational Training Centre",
+        "picture": twelve,
+        
+    },
+    {
+        "_id": "593e9297je17df20c4a237d42",
+        "index": 12,
+        "name": "Stride",
+        "picture": thirteen,
+        
+    },
+    {
+        "_id": "593e92d97e17df20c4a237d42",
+        "index": 13,
+        "name": "Technoserve",
+        "picture": forteen,
+        
+    }
+    ]
+}
+
+export default data;
